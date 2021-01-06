0a123b5e299424a7a99349ae9b8eca3abb699993
brianwlego <legobw@gmail.com>
2021-01-06 11:36:45 -0500

    refactoring promises to use async await

--- controllers/feeds_controller.js
+++ controllers/feeds_controller.js
@@ -4,135 +4,118 @@ const User = require('../models/user')
 
 
 //**  INDEX **//
-exports.indexPosts = (req, res, next) => {
-  Post.find().populate('creator')
-    .then(posts => {
-      res.status(200).json({
-        posts: posts
-      });
-    })
-    .catch(error => {
-      if (!error.statusCode){
-        error.statusCode = 500;
-      }
-      next(error);
-    })
+exports.indexPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find().populate('creator')
+    res.status(200).json({
+      posts: posts
+    });
+  } catch (error) {
+    if (!error.statusCode){
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 }
 
 //** SHOW **//
-exports.showPost = (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post){
-        const error = new Error('could not find post');
-        error.statusCode = 404;
-        throw error;
-      } else {
-        res.status(200).json({
-          post: post
-        })
-      }
-    })
-    .catch(error => {
-      if (!error.statusCode){
-        error.statusCode = 500;
-      }
-      next(error);
-    })
+exports.showPost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post){
+      const error = new Error('Post not found');
+      error.statusCode = 404;
+      throw error;
+    } else {
+      res.status(200).json({
+        post: post
+      })
+    }
+  } catch (error) {
+    if (!error.statusCode){
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 }
 
 //** CREATE **//
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()){
     const error = new Error('Validation failed, entered data is incorrect.')
     error.statusCode = 422;
     throw error;
   }
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imgUrl: '/images/book-img.jpeg',
-    creator: req.userId
-  })
-  let creator;
-  post.save()
-    .then(() => {
-      return User.findById(req.userId);
+  try {
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imgUrl: '/images/book-img.jpeg',
+      creator: req.userId
     })
-    .then(user => {
-      creator = user;
+    await post.save();
+    const user = await User.findById(req.userId);
       user.posts.push(post);
-      user.save();
-      return Post.findById(post._id).populate('creator')
+    await user.save();
+    const savedPost = await Post.findById(post._id).populate('creator');
+    res.status(201).json({
+      message: 'Post created succesfully!',
+      post: savedPost,
+      creator: {_id: user._id, name: user.name}
     })
-    .then((innerPost) => {
-      res.status(201).json({
-        message: 'Post created succesfully!',
-        post: innerPost,
-        creator: {_id: creator._id, name: creator.name}
-      })
-    })
-    .catch(error => {
-      if (!error.statusCode){
-        error.statusCode = 500;
-      }
-      next(error);
-    })
-
+  } catch (error) {
+    if (!error.statusCode){
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 }
 
 //** EDIT **//
-exports.updatePost = (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post){
-        const error = new Error('could not find post');
-        error.statusCode = 404;
-        throw error;
-      } else if (post.creator.toString() !== req.userId){
-        const err = new Error('Not authorized to change post')
-        err.statusCode = 403;
-        throw err;
-      } else {
-        post.title = req.body.title;
-        post.content = req.body.content;
-        return post.save();
-      }
-    })
-    .then(result => {
-      res.status(200).json({
-        post: result
-      })
-    })
-    .catch(error => {
-      if (!error.statusCode) error.statusCode = 500;
-      next(error);
-    })
+exports.updatePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post){
+      const error = new Error('could not find post');
+      error.statusCode = 404;
+      throw error;
+    } else if (post.creator.toString() !== req.userId){
+      const err = new Error('Not authorized to change post')
+      err.statusCode = 403;
+      throw err;
+    } else {
+      post.title = req.body.title;
+      post.content = req.body.content;
+      await post.save();
+      res.status(200).json({ post: post })
+    }
+  } catch (error){
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
 }
 
-exports.deletePost = (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post){
-        const error = new Error('could not find post');
-        error.statusCode = 404;
-        throw error;
-      } else if (post.creator.toString() !== req.userId){
-        const err = new Error('Not authorized to change post')
-        err.statusCode = 403;
-        throw err;
-      } else {
-        return Post.findByIdAndRemove(req.params.id)
-      }
-    })
-    .then(() => {
-      res.status(200).json({
-        message: 'Post was deleted.'
-      })
-    })
-    .catch(error => {
-      if (!error.statusCode) error.statusCode = 500;
-      next(error);
-    })
+exports.deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post){
+      const error = new Error('could not find post');
+      error.statusCode = 404;
+      throw error;
+    } else if (post.creator.toString() !== req.userId){
+      const err = new Error('Not authorized to change post')
+      err.statusCode = 403;
+      throw err;
+    } else {
+      await Post.findByIdAndRemove(req.params.id)
+      const user = await User.findById(req.userId)
+      user.posts.pull(post._id)
+      await user.save();
+      res.status(200).json({message: 'Post was deleted.'})
+    }
+  } catch (error){
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
 }
