88b29025f12334aba19d55245a0d59e16f7f3f29
brianwlego <legobw@gmail.com>
2021-01-06 11:47:12 -0500

    added async await to user controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 
-exports.createUser = (req, res, next) => {
+exports.createUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()){
     const error = new Error('Validation failed.');
@@ -12,54 +12,42 @@ exports.createUser = (req, res, next) => {
     error.data = errors.array();
     throw error;
   }
-  bcrypt.hash(req.body.password, 12)
-    .then(hashedPW => {
-      const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPW
-      })
-      return user.save()
+  try{
+    const hashedPW = await bcrypt.hash(req.body.password, 12)
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPW
     })
-    .then(result => {
-      res.status(201).json({msg: "User created!", userId: result._id})
-    })
-    .catch(error => {
-      if (!error.statusCode) error.statusCode = 500;
-      next(error);
-    })
+    await user.save()
+    res.status(201).json({msg: "User created!", userId: result._id})
+  } catch(error){
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
 }
 
-exports.login = (req, res, next) => {
-  let loadedUser;
-  User.findOne({email: req.body.email})
-    .then(user => {
-      if (!user){
-        const error = new Error('User with submitted email could not be found.')
-        error.statusCode = 404
-        throw error;
-      }
-      loadedUser = user;
-      return bcrypt.compare(req.body.password, user.password)
-    })
-    .then(isEqual => {
-      if (!isEqual){
-        const error = new Error('Incorrect password')
-        error.statusCode = 401;
-        throw error;
-      } 
-      const token = jwt.sign(
-        {
-          email: loadedUser.email,
-          userId: loadedUser._id.toString()
-        }, 
-        'secret', 
-        {expiresIn: '1h'}
-      );
-      res.status(200).json({ token: token, userId: loadedUser._id.toString() })
-    })
-    .catch(error => {
-      if (!error.statusCode) error.statusCode = 500;
-      next(error);
-    })
+exports.login = async(req, res, next) => {
+  try {
+    const user = await User.findOne({email: req.body.email})
+    if (!user){
+      const error = new Error('User with submitted email could not be found.')
+      error.statusCode = 404
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(req.body.password, user.password)
+    if (!isEqual){
+      const error = new Error('Incorrect password')
+      error.statusCode = 401;
+      throw error;
+    } 
+    const token = jwt.sign({
+      email: user.email,
+      userId: user._id.toString()
+    }, 'secret', {expiresIn: '1h'});
+    res.status(200).json({ token: token, userId: user._id.toString() })
+  } catch (error){
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
 }
